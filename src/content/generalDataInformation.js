9a6f002f90eeecb1d7b8e4f18380a3ab6d2455e0
santiagospinai7 <santiagospidrobo15@gmail.com>
2024-10-08 17:38:07 -0500

    banner informations added

---
+++ src/content/generalDataInformation.js
@@ -0,0 +1,16 @@
+const bannerInformation = {
+  tittle: 'Carillas Indirectas',
+  tecnique: 'Técnica MO',
+  ask: '¿Te gustaría dominar una técnica que te permita ofrecer resultados predecibles y estéticos a tus pacientes?',
+  description: 'En este curso online, te enseñaré paso a paso cómo construir carillas en resina indirecta. Un protocolo predecible y replicable.',
+  list: [
+    'Crea carillas con una estética natural.',
+    'Evita errores comunes en la técnica.',
+    'Trabajar inteligente, ganando más y dando mayor valor a tus pacientes.',
+  ],
+  buttonLabel: 'Quiero mi cupo',
+  buttonLink: 'https://api.whatsapp.com/send?phone=573204334436&text=Hola%20quiero%20mi%20cupo%20para%20el%20curso%20de%20Carillas%20Indirectas',
+};
+
+export default bannerInformation;
+  
