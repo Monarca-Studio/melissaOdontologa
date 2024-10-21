ca1fca1cc321de1c19111d8e8069ac134289d0d0
santiagospinai7 <santiagospidrobo15@gmail.com>
2024-10-21 15:01:24 -0500

    entrenamiento grabado

--- src/content/generalDataInformation.js
+++ src/content/generalDataInformation.js
@@ -5,10 +5,14 @@ import melissa_oscar_3 from '../assets/melissa_oscar_2.jpeg';
 import grupal_2 from '../assets/grupal_2.jpeg';
 import grupal from '../assets/grupal.jpeg';
 import meslissa_1 from '../assets/melissa_1.jpeg';
+import muestra_dientes from '../assets/muestra_dientes.jpg';
+import muestre_dientes_2 from '../assets/muestra_diente_2.jpg';
 
-import card_1 from '../assets/card_1.jpeg';
+import card_1 from '../assets/cursos_bg_2.jpg';
 import card_2 from '../assets/card_2.jpg';
 import card_3 from '../assets/card_3.jpg';
+
+import logo from '../assets/logo_mo.png';
 
 
 const bannerInformation = {
@@ -16,25 +20,42 @@ const bannerInformation = {
   tecnique: 'Técnica MO',
   ask: '¿Te gustaría dominar una técnica que te permita ofrecer resultados predecibles y estéticos a tus pacientes?',
   list: [
-    'Crea carillas con una estética natural.',
-    'Evita errores comunes en la técnica.',
-    'Trabajar inteligente, ganando más y dando mayor valor a tus pacientes.',
+    {
+      image: card_1,
+      text: 'Crea carillas con una estética natural',
+    },
+    {
+      image: card_2,
+      text: 'Evita errores comunes en la técnica.',
+    },
+    {
+      image: card_3,
+      text: 'Trabajar inteligente, ganando más y dando mayor valor a tus pacientes.',
+    },
   ],
   buttonLabel: 'Quiero mi cupo',
   buttonLink: 'https://pay.hotmart.com/A94698317I?off=gsjevx42',
 };
   
-const carrouselImages = [
+const carrouselImages1 = [
   {
     image: meslissa_1,
     alt: 'Melissa',
   },
   {
-    image: grupal,
-    alt: 'Grupo',
+    image: muestra_dientes,
+    alt: 'muestra_dientes',
   },
   {
-    image: grupal_2,
+    image: muestre_dientes_2,
+    alt: 'muestre_dientes_2',
+  },
+  {
+    image: oscar3,
+    alt: 'Oscar',
+  },
+  {
+    image: grupal,
     alt: 'Grupo',
   },
   {
@@ -42,16 +63,8 @@ const carrouselImages = [
     alt: 'Melissa y Oscar',
   },
   {
-    image: melissa_oscar,
-    alt: 'Melissa y Oscar',
-  },
-  {
-    image: oscar3,
-    alt: 'Oscar',
-  },
-  {
-    image: melissa_oscar_2,
-    alt: 'Melissa y Oscar',
+    image: grupal_2,
+    alt: 'Grupo',
   },
 ];
 
@@ -70,8 +83,48 @@ const cardsImages = [
   },
 ];
 
+const benefits = [
+  {
+    title: 'Resultados estéticos de alta calidad:',
+    text: 'Ofrece a tus pacientes carillas de resina indirecta con un acabado superior y natural. Mejora la estética dental con un brillo y color personalizado que garantiza sonrisas perfectas.',
+    image: logo,
+    alt: 'Resultados estéticos de alta calidad',
+  },
+  {
+    title: 'Mayor durabilidad y resistencia:',
+    text: 'Las carillas indirectas están diseñadas para resistir el desgaste diario, garantizando un tratamiento duradero y de bajo mantenimiento, lo que genera mayor satisfacción a largo plazo para tus pacientes.',
+    image: logo,
+    alt: 'Mayor durabilidad y resistencia',
+  },
+  {
+    title: 'Optimización del tiempo clínico:',
+    text: 'Reduce el tiempo de intervención directa en el sillón dental. Con las carillas indirectas, el trabajo se realiza en el laboratorio, lo que te permite atender más pacientes sin comprometer la calidad.',
+    image: logo,
+    alt: 'Optimización del tiempo clínico',
+  },
+  {
+    title: 'Menor sensibilidad postoperatoria para el paciente:',
+    text: 'Minimiza la incomodidad y sensibilidad en tus pacientes gracias a la colocación más precisa y controlada de las carillas de resina indirecta. Una experiencia más cómoda para todos.',
+    image: logo,
+    atl: 'Menor sensibilidad postoperatoria para el paciente',
+  },
+  {
+    title: 'Soluciones personalizadas para casos complejos:',
+    text: 'Resuelve problemas estéticos y funcionales complejos con mayor precisión. Las carillas indirectas te permiten ofrecer soluciones personalizadas para cada caso, desde fracturas hasta desgaste dental.',
+    image: logo,
+    alt: 'Soluciones personalizadas para casos complejos',
+  },
+  {
+    title: 'Aumenta el valor percibido de tus tratamientos:',
+    text: 'Diferénciate de la competencia ofreciendo carillas de resina indirecta, un tratamiento estético avanzado que no solo mejora la sonrisa de tus pacientes, sino que también incrementa el valor percibido de tu consulta.” Estos puntos están diseñados para captar la atención de odontólogos que buscan mejorar su oferta de tratamientos estéticos y optimizar el rendimiento de su clínica.',
+    image: logo,
+    alt: 'Aumenta el valor percibido de tus tratamientos',
+  },
+];
+
 export { 
-  carrouselImages,
+  carrouselImages1,
   bannerInformation,
-  cardsImages
+  cardsImages,
+  benefits
 };
